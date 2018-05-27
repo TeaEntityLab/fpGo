d6628798527fe99d3b0434d3bbd19c349e5f08e2
John Lee <johnteee@gmail.com>
2018-05-27 11:04:39 +0800

    Remove continual cases of MonadIO

--- monadIO.go
+++ monadIO.go
@@ -1,9 +1,7 @@
 package fpGo
 
 type MonadIODef struct {
-	effect  func() *interface{}
-	origin  *MonadIODef
-	subList []*Subscription
+	effect func() *interface{}
 }
 type Subscription struct {
 	OnNext func(*interface{})
@@ -18,46 +16,23 @@ func (self MonadIODef) Just(in *interface{}) *MonadIODef {
 	}}
 }
 
-func (self *MonadIODef) FlatMap(fn func(*interface{}) MonadIODef) *MonadIODef {
+func (self *MonadIODef) FlatMap(fn func(*interface{}) *MonadIODef) *MonadIODef {
 
 	return &MonadIODef{effect: func() *interface{} {
 		next := fn(self.doEffect())
-		next.setOrigin(self.GetOrigin())
 		return next.doEffect()
-	}, origin: self.GetOrigin()}
+	}}
 
 }
 func (self *MonadIODef) Subscribe(s Subscription) *Subscription {
 	if s.OnNext != nil {
-		origin := self.GetOrigin()
-		origin.AddSubscription(&s)
-		origin.doEffect()
+		s.OnNext(self.doEffect())
 	}
 
 	return &s
 }
-func (self *MonadIODef) AddSubscription(s *Subscription) {
-	self.subList = append(self.subList, s)
-}
-func (self MonadIODef) GetSubscriptionList() []*Subscription {
-	return self.subList
-}
-func (self MonadIODef) setOrigin(m *MonadIODef) {
-	self.origin = m
-}
-func (self *MonadIODef) GetOrigin() *MonadIODef {
-	if self.origin == nil {
-		return self
-	}
-
-	return self.origin
-}
 func (self *MonadIODef) doEffect() *interface{} {
-	v := self.effect()
-	for _, s := range self.GetSubscriptionList() {
-		s.OnNext(v)
-	}
-	return v
+	return self.effect()
 }
 
 var MonadIO MonadIODef
--- monadIO_test.go
+++ monadIO_test.go
@@ -18,4 +18,16 @@ func TestMonadIO(t *testing.T) {
 		},
 	})
 	assert.Equal(t, 1, actualInt)
+
+	m = MonadIO.JustVal(1).FlatMap(func(in *interface{}) *MonadIODef {
+		v, _ := Monad.Just(in).ToInt()
+		return MonadIO.JustVal(v + 1)
+	})
+	actualInt = 0
+	m.Subscribe(Subscription{
+		OnNext: func(in *interface{}) {
+			actualInt, _ = Monad.Just(in).ToInt()
+		},
+	})
+	assert.Equal(t, 2, actualInt)
 }
