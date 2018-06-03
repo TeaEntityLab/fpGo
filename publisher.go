e3503f3346759815817417233392b0f80ed723b1
John Lee <johnteee@gmail.com>
2018-06-03 12:12:37 +0800

    Implement Publisher & tests

---
+++ publisher.go
@@ -0,0 +1,88 @@
+package fpGo
+
+import "sync"
+
+type PublisherDef struct {
+	subscribers []*Subscription
+	subscribeM  sync.Mutex
+	subOn       *HandlerDef
+
+	origin *PublisherDef
+}
+
+func (self *PublisherDef) New() *PublisherDef {
+	p := &PublisherDef{}
+
+	return p
+}
+
+func (self *PublisherDef) FlatMap(fn func(*interface{}) *interface{}) *PublisherDef {
+	next := self.New()
+	next.origin = self
+	self.Subscribe(Subscription{
+		OnNext: func(in *interface{}) {
+			next.Publish(fn(in))
+		},
+	})
+
+	return next
+}
+func (self *PublisherDef) Subscribe(sub Subscription) *Subscription {
+	s := &sub
+
+	self.doSubscribeSafe(func() {
+		self.subscribers = append(self.subscribers, s)
+	})
+	return s
+}
+func (self *PublisherDef) SubscribeOn(h *HandlerDef) *PublisherDef {
+	self.subOn = h
+	return self
+}
+func (self *PublisherDef) Unsubscribe(s *Subscription) {
+	isAnyMatching := false
+
+	self.doSubscribeSafe(func() {
+		subscribers := self.subscribers
+		for i, v := range subscribers {
+			if v == s {
+				isAnyMatching = true
+				subscribers = append(subscribers[:i], subscribers[i+1:]...)
+				self.subscribers = subscribers
+				break
+			}
+		}
+	})
+
+	// Delete subscriptions recursively
+	if isAnyMatching {
+		self.Unsubscribe(s)
+	}
+}
+func (self *PublisherDef) Publish(result *interface{}) {
+	var subscribers []*Subscription
+	self.doSubscribeSafe(func() {
+		subscribers = self.subscribers
+	})
+
+	for _, s := range subscribers {
+		if s.OnNext != nil {
+
+			doSub := func() {
+				s.OnNext(result)
+			}
+			if self.subOn != nil {
+				self.subOn.Post(doSub)
+			} else {
+				doSub()
+			}
+		}
+	}
+}
+func (self *PublisherDef) doSubscribeSafe(fn func()) {
+	self.subscribeM.Lock()
+	fn()
+	self.subscribeM.Unlock()
+}
+
+var Publisher PublisherDef
---
+++ publisher_test.go
@@ -0,0 +1,67 @@
+package fpGo
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPublisher(t *testing.T) {
+	var s *Subscription
+	p := Publisher.New()
+	p2 := p
+
+	var actual = 0
+	var expected = 0
+	assert.Equal(t, expected, actual)
+	assert.Equal(t, true, s == nil)
+
+	actual = 0
+	expected = 0
+	s = p.Subscribe(Subscription{
+		OnNext: func(in *interface{}) {
+			// fmt.Println(*in)
+			actual, _ = Monad.Just(in).ToInt()
+		},
+	})
+	assert.Equal(t, expected, actual)
+	assert.Equal(t, false, s == nil)
+
+	actual = 0
+	expected = 1
+	p.Publish(PtrOf(1))
+	assert.Equal(t, expected, actual)
+
+	actual = 0
+	expected = 0
+	p.Unsubscribe(s)
+	p.Publish(PtrOf(1))
+	assert.Equal(t, expected, actual)
+
+	p = Publisher.New()
+	p2 = p.FlatMap(func(in *interface{}) *interface{} {
+		v, _ := Monad.Just(in).ToInt()
+		return PtrOf(v + 2)
+	}).FlatMap(func(in *interface{}) *interface{} {
+		v, _ := Monad.Just(in).ToInt()
+		return PtrOf(v + 3)
+	})
+	s = p2.Subscribe(Subscription{
+		OnNext: func(in *interface{}) {
+			actual, _ = Monad.Just(in).ToInt()
+		},
+	})
+	actual = 0
+	expected = 6
+	p.Publish(PtrOf(1))
+	assert.Equal(t, expected, actual)
+	actual = 0
+	expected = 16
+	p.Publish(PtrOf(11))
+	assert.Equal(t, expected, actual)
+	actual = 0
+	expected = 0
+	p2.Unsubscribe(s)
+	p.Publish(PtrOf(1))
+	assert.Equal(t, expected, actual)
+}
