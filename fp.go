e03857655e408d26aa192341f53d806fb6832031
JunYi JohnTeee Lee <johnteee@gmail.com>
2018-06-20 21:59:28 +0800

    gofmt proceeded

--- fp.go
+++ fp.go
@@ -12,7 +12,7 @@ type fnObj func(interface{}) interface{}
 func Compose(fnList ...fnObj) fnObj {
 	return func(s interface{}) interface{} {
 		f := fnList[0]
-		nextFnList := fnList[1:len(fnList)]
+		nextFnList := fnList[1:]
 
 		if len(fnList) == 1 {
 			return f(s)
